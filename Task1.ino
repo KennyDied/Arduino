2d16b55c34e452be6f1bad157b2e8ee0933e68fa
Danila <55344507+KennyDied@users.noreply.github.com>
2019-09-22 14:50:21 +0300

    Update Task1.ino

--- Task1.ino
+++ Task1.ino
@@ -1,4 +1,5 @@
-//Задачка на 25 баллов.
+//Задачка на 35 баллов.
+//Горит зелёный, а ты берёшь, нажимаешь кнопку и начинают гореть жёлтый, а потом красный (c)Андрей Бронников.
 
 void setup() {
 
@@ -7,21 +8,26 @@ pinMode(5, OUTPUT); // Красный светодиод
 pinMode(6, OUTPUT); // Желтый светодиод
 pinMode(7, OUTPUT); // Зелетый светодиод
 
+pinMode(3,INPUT_PULLUP); // Кнопка 
+
 }
 
 void loop() {
+ 
+  boolean button =  !digitalRead(3);
 
-digitalWrite(5, HIGH);
-delay(1000);
-digitalWrite(5, LOW);
-digitalWrite(6, HIGH);
-delay(1000);
-digitalWrite(6, LOW);
-digitalWrite(7, HIGH);
-delay(1000);
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH);
-delay(1000);
-digitalWrite(6, LOW);
+if(button == 1){
+  digitalWrite (7, LOW);
+  digitalWrite (6, HIGH);
+  delay (2000);
+  digitalWrite (6, LOW);
+  digitalWrite (5, HIGH);
+  delay (5000);
+  digitalWrite(5, LOW);
+}
+
+else{
+  digitalWrite(7, HIGH);
+}
 
 }
