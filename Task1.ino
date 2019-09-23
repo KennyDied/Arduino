c0483c9c79da6f3f1d5acdde77804ba4842455b2
Danila <55344507+KennyDied@users.noreply.github.com>
2019-09-23 21:14:04 +0300

    Update Task1.ino

--- Task1.ino
+++ Task1.ino
@@ -1,4 +1,5 @@
-//Задачка на 45 баллов.
+//Задачка на 45 баллов. 
+//Итоговый вариант, добавил кнопку
 
 void setup() {
   Serial.begin(9600);
@@ -8,9 +9,25 @@ void setup() {
 
   pinMode(A7, INPUT); // Переменный резистор 
 
+  pinMode(3,INPUT_PULLUP); // Кнопка 
+
 }
 
 void loop() {
+
+  boolean button =  !digitalRead(3);
+
+if(button == 1){                 // Тык по кнопке
+  digitalWrite (7, LOW);
+  digitalWrite (6, HIGH);        // Загорается желтый на 2 секунды
+  delay (2000);
+  digitalWrite (6, LOW);         
+  digitalWrite (5, HIGH);        //  А теперь горит красный 5 секунд
+  delay (5000);
+  digitalWrite(5, LOW);
+  }
+
+else {
   int speed = analogRead(A7); // Считываю значения с резистора
   digitalWrite(5, HIGH);
   delay(speed);
@@ -21,5 +38,5 @@ void loop() {
   digitalWrite(7, HIGH);
   delay(speed);
   digitalWrite(7, LOW);
-  
+   }
 }
