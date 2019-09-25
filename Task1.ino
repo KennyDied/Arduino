64206235096137d469fe8adbeac1d7b59ef371d6
Danila <55344507+KennyDied@users.noreply.github.com>
2019-09-25 20:50:57 +0300

    Update Task1.ino

--- Task1.ino
+++ Task1.ino
@@ -1,6 +1,6 @@
 //Задачка на 45 баллов. 
-//Итоговый вариант, добавил кнопку
-//Вторая попытка 
+//Итоговый вариант, пробую сделать кнопку через interrupt 
+//Третья попытка 
 
 void setup() {
   Serial.begin(9600);
@@ -11,26 +11,15 @@ void setup() {
   pinMode(A7, INPUT); // Переменный резистор 
 
   pinMode(3,INPUT_PULLUP); // Кнопка 
-
+  
+  attachInterrupt(1, stopTrafficLight, FALLING); // Прерываю работу светофора по нажатию 
 }
 
 void loop() {
-
-  boolean button =  !digitalRead(3);
-
-if(button == 1){                 // Тык по кнопке
-  digitalWrite (6, HIGH);        // Загорается желтый на 5 секунд
-  delay (5000);
-  digitalWrite (6, LOW);         
-  digitalWrite (5, HIGH);        //  А теперь горит красный 10 секунд
-  delay (10000);
-  digitalWrite(5, LOW);
-  }
-
-else {
-  int speed = analogRead(A7); // Считываю значения с резистора
   
-  digitalWrite(5, HIGH);
+  int speed = analogRead(A7);   // Считываю значения с резистора, меняю скорость светофора
+  
+  digitalWrite(5, HIGH); 
   delay(speed);
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
@@ -39,5 +28,17 @@ else {
   digitalWrite(7, HIGH);
   delay(speed);
   digitalWrite(7, LOW);
-   }
+   
+}
+
+
+void stopTrafficLight() {        // Прервал работу фонаря нажатием кнопки  
+ 
+  digitalWrite (6, HIGH);        // Загорается желтый на 5 секунд
+  delay (5000);
+  digitalWrite (6, LOW);         
+  digitalWrite (5, HIGH);        //  А теперь горит красный 10 секунд
+  delay (10000);
+  digitalWrite(5, LOW);
+    
 }
